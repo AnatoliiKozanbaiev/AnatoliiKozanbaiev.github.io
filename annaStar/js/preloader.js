278971e7ab2e7d2af62f478e2d92f099670d5354
AnatoliiKozanbaiev <anatoliykozanbaev@gmail.com>
2017-04-12 03:08:01 +0300

    added preloader

--- annaStar/js/preloader.js
+++ annaStar/js/preloader.js
@@ -2,5 +2,5 @@ $(window).on('load', function () {
 	var $preloader = $('#p_prldr'),
 	$svg_anm   = $preloader.find('.svg_anm');
 	$svg_anm.fadeOut();
-	$preloader.delay(300).fadeOut('slow');
+	$preloader.delay(400).fadeOut('slow');
 });
